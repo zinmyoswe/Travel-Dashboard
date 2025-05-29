b384b55b8dbef6fba5c4aa44d4326c11e95905d9
zinmyoswe <overmidnight12@gmail.com>
2025-05-29 18:39:48 +0700

    add basic route

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,6 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    route( 'dashboard', 'routes/admin/dashboard.tsx'),
+    route( 'zin', 'routes/admin/zin.tsx')
+] satisfies RouteConfig;
---
+++ app/routes/admin/dashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const dashboard = () => {
+  return (
+    <div>dashboard page</div>
+  )
+}
+
+export default dashboard
