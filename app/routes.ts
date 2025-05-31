718755d59f97d265d45445737dd974997d42a841
zinmyoswe <overmidnight12@gmail.com>
2025-05-31 14:56:31 +0700

    create sign-in page and route

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,7 @@
 import { type RouteConfig, route, layout } from "@react-router/dev/routes";
 
 export default [
+    route('sign-in', 'routes/root/sign-in.tsx'),
     layout("routes/admin/admin-layout.tsx", [
         route( 'dashboard', 'routes/admin/dashboard.tsx'),
         route( 'all-users', 'routes/admin/all-users.tsx'),
