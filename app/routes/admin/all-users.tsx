19e1cf090093a12555fc02ec0155261e96dcb59a
zinmyoswe <overmidnight12@gmail.com>
2025-06-01 22:16:46 +0700

    get all-users data from index.ts

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,8 +1,20 @@
+import { Header } from 'components'
 import React from 'react'
+import {GridComponent} from "@syncfusion/ej2-react-grids";
+import { users } from '~/constants'
 
 const AllUsers = () => {
   return (
-    <div>all-users</div>
+    <main className='all-users wrapper'>
+      <Header 
+        title='Manage Users'
+        description='filter, sort, and access detailed user profiles'
+      />
+
+      <GridComponent dataSource={users}>
+
+      </GridComponent>
+    </main>
   )
 }
 
