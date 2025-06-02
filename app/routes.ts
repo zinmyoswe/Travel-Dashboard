fab2c7f6d401073d6c9be839dd219d935ce8e1c1
zinmyoswe <overmidnight12@gmail.com>
2025-06-03 01:45:51 +0700

    finish generativeAI trip

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, route, layout } from "@react-router/dev/routes";
 
 export default [
     route('sign-in', 'routes/root/sign-in.tsx'),
+    route('api/create-trip', 'routes/api/create-trip.ts'),
     layout("routes/admin/admin-layout.tsx", [
         route( 'dashboard', 'routes/admin/dashboard.tsx'),
         route( 'all-users', 'routes/admin/all-users.tsx'),
--- app/routes/admin/create-trip.tsx
+++ app/routes/admin/create-trip.tsx
@@ -68,8 +68,26 @@ const CreateTrip = ({ loaderData }: Route.ComponentProps) => {
         }
 
         try{
-            console.log('user',user);
-            console.log('formData', formData);
+            // console.log('user',user);
+            // console.log('formData', formData);
+            const response = await fetch('/api/create-trip', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    country: formData.country,
+                    numberOfDays: formData.duration,
+                    travelStyle: formData.travelStyle,
+                    interests: formData.interest,
+                    budget: formData.budget,
+                    groupType: formData.groupType,
+                    userId: user.$id
+                })
+            })
+
+            const result: CreateTripResponse = await response.json();
+
+            if(result?.id) navigate(`/trips/${result.id}`)
+            else console.error('Failed to generate a trip')
         } catch (e) {
             console.error('Error generating trip', e);
         } finally {
